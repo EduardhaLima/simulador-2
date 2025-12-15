20c879d2dbd0aef519d715148126b7b8628d4d0a
EduardhaLima <eduardharflimabr@gmail.com>
2025-12-15 14:53:27 -0300

    botao

--- desafio-manutencao-eduardha/index.js
+++ desafio-manutencao-eduardha/index.js
@@ -1,3 +1,19 @@
-function avisar(){
-    alert("Você clicou no botão!");
+function msg() {
+    alert('Você clicou no botão!');
 }
+
+function clicked() {
+    alert('elemento clicado');
+}
+
+function addEvents() {
+    const button = document.getElementById('botao'); 
+
+    if (button) {
+        button.addEventListener('click', clicked); 
+    } else {
+        console.error("Elemento com ID 'botao' não encontrado!");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', addEvents);
